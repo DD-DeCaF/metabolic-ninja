2e0d4ef118e0fca24c9ed7b899d044b154ab598e
Ali Kaafarani <ali@kvikshaug.no>
2019-07-10 15:07:11 +0200

    feat: configure the model for cameo

--- src/worker/data.py
+++ src/worker/data.py
@@ -27,6 +27,7 @@ class Job:
     def __init__(
         self,
         model,
+        biomass_reaction_id,
         product_name,
         max_predictions,
         aerobic,
@@ -38,7 +39,26 @@ class Job:
         user_name,
         user_email,
     ):
+        # Configure the model object for cameo.
         self.model = model
+        # FIXME (Moritz Beber): We should allow users to specify a medium that they
+        #  previously
+        # uploaded.
+        self.model.solver = "cplex"
+        # FIXME (Moritz Beber): This uses BiGG notation to change the lower bound
+        #  of the exchange reaction. Should instead find this using a combination of
+        #  metabolites in the `model.exchanges`, MetaNetX identifiers and/or
+        #  metabolite formulae. Then set this on the `model.medium` to be sure
+        #  about exchange direction.
+        if not aerobic and "EX_o2_e" in self.model.reactions:
+            self.model.reactions.EX_o2_e.lower_bound = 0
+        self.model.biomass = biomass_reaction_id
+        # FIXME (Moritz Beber): We can try to be smart, as in theoretical yield
+        #  app, but ideally the carbon source is user defined just like
+        #  default_biomass_reaction. Maybe we need a new field for the medium
+        #  database model?
+        self.model.carbon_source = "EX_glc__D_e"
+
         self.product_name = product_name
         self.max_predictions = max_predictions
         self.aerobic = aerobic
@@ -56,6 +76,7 @@ class Job:
         logger.debug("Deserializing job parameters")
         return Job(
             cobra.io.model_from_dict(params["model"]["model_serialized"]),
+            params["model"]["default_biomass_reaction"],
             params["product_name"],
             params["max_predictions"],
             params["aerobic"],
