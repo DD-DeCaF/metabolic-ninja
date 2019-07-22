bdafdf94c897d6b546dcf7caf377d43002939f4a
Ali Kaafarani <ali@kvikshaug.no>
2019-07-22 11:11:30 +0200

    refactor: use compact json

--- src/metabolic_ninja/rabbitmq.py
+++ src/metabolic_ninja/rabbitmq.py
@@ -23,7 +23,7 @@ import pika
 
 def submit_job(**kwargs):
     """Submit a new job to the message queue."""
-    message = json.dumps(kwargs)
+    message = json.dumps(kwargs, separators=(",", ":"))
     with pika.BlockingConnection(
         pika.ConnectionParameters(host=os.environ["RABBITMQ_HOST"])
     ) as connection:
